d0a6a05b897594d7c7a4f38233dafd228c7b755c
Madhusudan Nayak <madhusudannayak80@gmail.com>
2021-09-09 17:39:06 +0530

    update

--- app/src/main/java/com/example/googlesignin/dashboard/ui/DashboardFragment.kt
+++ app/src/main/java/com/example/googlesignin/dashboard/ui/DashboardFragment.kt
@@ -5,16 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.example.googlesignin.R
+import com.example.googlesignin.databinding.FragmentDashboardBinding
+import com.example.googlesignin.databinding.FragmentLoginBinding
 
 class DashboardFragment : Fragment() {
+    lateinit var binding: FragmentDashboardBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dashboard, container, false)
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_login, container, false
+        )
+        return binding.root
     }
 
 }
